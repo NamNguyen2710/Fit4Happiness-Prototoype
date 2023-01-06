d1bde98283a22f55f72d11f695a6bd204e28a0d8
NamNguyen2710 <nam.nguyen@velox.no>
2023-01-06 17:14:03 +0700

    update about us and user schedule

--- src/modules/AboutUs/AboutUs.js
+++ src/modules/AboutUs/AboutUs.js
@@ -1,37 +1,63 @@
 function AboutUs() {
   return (
     <div>
-      <h3>About Us</h3>
-      <p>
-        Our team created a website named fit4happiness.site. Our focus is
-        fitness, more specifically bodybuilding. Our motivation comes from the
-        backstory of Duong Tran, our lead manager, who overcame obesity as a
-        teenager, with many difficulties in his training. Therefore, Duong
-        himself and the Fit4Happiness team want to help new fitness trainers to
-        have a more visible pathway to obtain consistent results. We name our
-        site and company, Fit4Happiness, as we believe a healthy lifestyle is
-        the fundamental material to create a life filled with happiness.{" "}
-      </p>
-      <div>
+      <section>
         <div>
-          <img />
-          <h4>DUONG TRAN</h4>
-          <h4>Lead Manager</h4>
+          <h1>About Us</h1>
+          <p>Dear users,</p>
+          <p>
+            Our team created a website named fit4happiness.site. Our focus is
+            fitness, more specifically bodybuilding. Our motivation comes from
+            the backstory of Duong Tran, our lead manager, who overcame obesity
+            as a teenager, with many difficulties in his training. Therefore,
+            Duong himself and the Fit4Happiness team want to help new fitness
+            trainers to have a more visible pathway to obtain consistent
+            results. We name our site and company, Fit4Happiness, as we believe
+            a healthy lifestyle is the fundamental material to create a life
+            filled with happiness.
+          </p>
+          <p>- Fit4Happiness team</p>
         </div>
         <div>
           <img />
-          <h4>NAM NGUYEN</h4>
-          <h4>Tech Lead</h4>
+        </div>
+      </section>
+      <section>
+        <h2>Our Team Members</h2>
+        <div>
+          <img />
+          <h5>DUONG TRAN</h5>
+          <p className='subtitle-1'>Lead Manager</p>
+          <p className='subtitle-1'>
+            " Spending his teens in the gym but wasted a lot of time in the
+            wrong direction, he would love to help new trainers to avoid
+            mistakes and optimize the training process. "
+          </p>
         </div>
         <div>
           <img />
-          <h4>ANH LUU</h4>
-          <h4>Design Lead</h4>
+          <h5>NAM NGUYEN</h5>
+          <p className='subtitle-1'>Tech Lead</p>
+          <p className='subtitle-1'>
+            " Nam believes that fitness is an important and prominent subject,
+            especially for the young generation. It is beneficial if there is an
+            application that can help tracking, analyze progress and advise a
+            suitable practice schedule for specific body types and
+            goals.•Relevant skills & Background: . "
+          </p>
         </div>
-      </div>
-      <h4>
-        FIT<span>4</span>HAPPINESS
-      </h4>
+        <div>
+          <img />
+          <h5>ANH LUU</h5>
+          <p className='subtitle-1'>Design Lead</p>
+          <p className='subtitle-1'>
+            " Fitness has been one of the main concerns of people for centuries.
+            Learning how to stay healthy through exercise and eating right can
+            be confusing in this modern age. That is why Anh believes there
+            would always be a need for a digital fitness tracker/helper. "
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
